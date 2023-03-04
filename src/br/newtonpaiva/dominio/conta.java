7abf16f12fd5187fbcb6fed734d4ebc3016af33c
11513288 <cleissilima@gmail.com>
2023-03-03 21:59:00 -0300

    cria construtor/equals/hash/transferir

--- src/br/newtonpaiva/dominio/conta.java
+++ src/br/newtonpaiva/dominio/conta.java
@@ -1,10 +1,25 @@
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class conta  {
     private Integer numero;
 
     private Double saldo;
 
+ //construtor
+    public conta(){
+        this(null);
+    }
+//construtor
+    public conta(Integer numero){
+        this(numero, 0.0);
+    }
+//construtor
+    public conta(Integer numero, Double saldo){
+        this.numero = numero;
+        this.saldo = saldo;
+    }
     public Double sacar(Double valor){
 
         return 0.0;
@@ -14,6 +29,17 @@ public class conta  {
             throw new IllegalArgumentException("depósito mínimo de 1 centavo");
         saldo += valor;
         return saldo;
+    }
+    public void transferir(conta destino, Double valor){
+        if(this.getSaldo() < valor)
+            throw new IllegalArgumentException( "Invalido");
+
+        if(this.equals(destino))
+            throw new IllegalArgumentException("Invalido");
+        this.sacar(valor);
+        destino.depositar(valor);
+
+
     }
     public Integer getNumero(){
 
@@ -29,14 +55,23 @@ public class conta  {
 
         this.saldo = saldo;
     }
-    public Double getSaldo(){
+    public Double getSaldo() {
 
         return saldo;
-
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        conta conta = (conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
+    }
 }
 
 
